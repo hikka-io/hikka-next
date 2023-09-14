7e748b826de6ec05b02254d0eda8d2c32c9263c0
olexh <typicaladmsky@gmail.com>
2023-09-15 02:13:25 +0300

    update prettier, eslint, tsconfig

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    importOrder: ["react[^-/]", "^next/(.*)$", "@[^/](.*)$", "@/(.*)$", "^[./]"]
+}
