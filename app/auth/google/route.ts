79e7c7e0630644cfab74657292a551a54a92a686
olexh <typicaladmsky@gmail.com>
2023-11-17 22:54:35 +0200

    add google auth

--- app/auth/google/route.ts
+++ app/auth/google/route.ts
@@ -1,6 +1,6 @@
 import loginOAuth from '@/utils/api/auth/loginOAuth';
 import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
+// import { cookies } from 'next/headers';
 
 export async function GET(request: Request) {
     try {
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
             provider: 'google',
         });
 
-        cookies().set('secret', res.secret);
+        // cookies().set('secret', res.secret);
 
         return redirect('/anime?auth=success&provider=google');
     } catch (e) {
