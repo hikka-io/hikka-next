af4ae655d0bddb6e5e5b7ccdbbe57024ec3af198
olexh <typicaladmsky@gmail.com>
2023-11-20 14:11:28 +0200

    fix mobile double-clicking

--- utils/provider.tsx
+++ utils/provider.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import React, { PropsWithChildren, useEffect } from 'react';
+import React, {PropsWithChildren, useEffect} from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import AuthProvider from '@/utils/providers/AuthProvider';
 import ModalProvider from '@/utils/providers/ModalProvider';
-import { usePathname, useSearchParams } from 'next/navigation';
-import NProgress from 'nprogress';
+import { SnackbarProvider } from 'notistack';
+import {usePathname, useSearchParams} from "next/navigation";
+import NProgress from "nprogress";
 
 interface Props extends PropsWithChildren {}
 
@@ -25,14 +26,16 @@ function Providers({ children }: Props) {
     }, [pathname, searchParams]);
 
     return (
-        <ModalProvider>
-            <AuthProvider>
-                <QueryClientProvider client={client}>
-                    {children}
-                    <ReactQueryDevtools initialIsOpen={false} />
-                </QueryClientProvider>
-            </AuthProvider>
-        </ModalProvider>
+        <SnackbarProvider preventDuplicate>
+            <ModalProvider>
+                <AuthProvider>
+                    <QueryClientProvider client={client}>
+                        {children}
+                        <ReactQueryDevtools initialIsOpen={false} />
+                    </QueryClientProvider>
+                </AuthProvider>
+            </ModalProvider>
+        </SnackbarProvider>
     );
 }
 
