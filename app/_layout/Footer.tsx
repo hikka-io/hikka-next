a49db9878ac1c64cbd752297c791eace6d48c57b
olexh <typicaladmsky@gmail.com>
2023-11-29 00:30:08 +0200

    fix footer

--- app/_layout/Footer.tsx
+++ app/_layout/Footer.tsx
@@ -13,7 +13,7 @@ const Component = () => {
         <footer className="w-full mt-12 border-t border-t-secondary">
             <div className="container max-w-screen-xl mx-auto p-4">
                 <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
-                    <div className="flex gap-4 flex-wrap justify-center">
+                    <div className="flex gap-4 flex-wrap justify-center lg:justify-start">
                         <Link href="https://t.me/hikka_io" target="_blank" className="btn btn-ghost btn-sm">
                             <MingcuteTelegramFill />
                             Telegram
