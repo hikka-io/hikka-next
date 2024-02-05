5950f7801f6c3f5e9485be7876a2a2c1a86debd3
olexh <typicaladmsky@gmail.com>
2024-02-05 23:49:31 +0200

    add memo to markdown

--- app/_components/markdown/viewer/MD-viewer.tsx
+++ app/_components/markdown/viewer/MD-viewer.tsx
@@ -1,20 +1,21 @@
 'use client';
 
-import React from 'react';
+import React, { memo } from 'react';
 import Markdown, { Options } from 'react-markdown';
 import remarkDirective from 'remark-directive';
 import remarkDirectiveRehype from 'remark-directive-rehype';
-import Spoiler from './_components/spoiler';
-import Link from './_components/link';
-import remarkMentions from './plugins/remark-mentions';
+
 import { cn } from '@/utils';
+
+import Spoiler from './_components/spoiler';
+import remarkMentions from './plugins/remark-mentions';
 
 interface Props extends Options {}
 
 const Component = ({ children, className, ...props }: Props) => {
     return (
         <Markdown
-            className={cn("markdown w-full", className)}
+            className={cn('markdown w-full', className)}
             // disallowedElements={['code']}
             remarkPlugins={[
                 remarkDirective,
@@ -27,9 +28,9 @@ const Component = ({ children, className, ...props }: Props) => {
             components={{
                 spoiler: Spoiler,
                 a: ({ node, children }) => (
-                    <Link href={(node?.properties?.href as string) || ''}>
+                    <a href={(node?.properties?.href as string) || ''}>
                         {children}
-                    </Link>
+                    </a>
                 ),
                 code: ({ node, children }) => <p>{children}</p>,
             }}
@@ -40,4 +41,4 @@ const Component = ({ children, className, ...props }: Props) => {
     );
 };
 
-export default Component;
+export default memo(Component);
