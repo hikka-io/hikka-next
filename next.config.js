b09b7566c0f174c08c569d366db8f4cdc0f0cdf7
olexh <typicaladmsky@gmail.com>
2024-02-27 03:20:04 +0200

    add image optimization

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'cdn.hikka.io',
+                hostname: '**',
                 port: '',
                 pathname: '**',
             },
