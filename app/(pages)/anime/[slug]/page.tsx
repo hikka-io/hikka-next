e6604b3ff16eb1042b1b578e482226832fec7a8c
olexh <typicaladmsky@gmail.com>
2024-03-30 21:48:48 +0200

    fix typo

--- app/(pages)/anime/[slug]/page.tsx
+++ app/(pages)/anime/[slug]/page.tsx
@@ -10,7 +10,7 @@ import Media from './components/media';
 import Staff from './components/staff';
 import WatchStats from './components/watch-stats/watch-stats';
 
-const AnimePage = ({ params }: { params: Record<string, string> }) => {
+const AnimePage = () => {
     return (
         <div className="grid grid-cols-1 gap-12 lg:grid-cols-[1fr_33%] lg:gap-16 xl:grid-cols-[1fr_30%]">
             <div className="relative order-2 flex flex-col gap-12 lg:order-1">
@@ -22,14 +22,14 @@ const AnimePage = ({ params }: { params: Record<string, string> }) => {
                 <Links />
                 <div className="flex flex-col gap-12 lg:hidden">
                     <WatchStats />
-                    <Followings slug={params.slug} />
+                    <Followings />
                 </div>
             </div>
             <div className="order-1 flex flex-col gap-12 lg:order-2">
                 <About />
                 <div className="hidden lg:flex lg:flex-col lg:gap-12">
                     <WatchStats />
-                    <Followings slug={params.slug} />
+                    <Followings />
                 </div>
             </div>
         </div>
