85bebd702234b48c2ddd8613899933bd56832301
olexh <typicaladmsky@gmail.com>
2024-05-04 03:53:01 +0300

    enable useUser hook when username is passed

--- services/hooks/user/useUser.ts
+++ services/hooks/user/useUser.ts
@@ -2,10 +2,11 @@ import { useQuery } from '@tanstack/react-query';
 
 import getUserInfo from '@/services/api/user/getUserInfo';
 
-const useUser = ({ username }: { username: string }) => {
+const useUser = ({ username }: { username?: string }) => {
     return useQuery({
         queryKey: ['user', username],
-        queryFn: () => getUserInfo({ params: { username } }),
+        queryFn: () => getUserInfo({ params: { username: String(username) } }),
+        enabled: !!username,
     });
 };
 
