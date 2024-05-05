0b1ce26d83dacfd6b0bbde883a4b4a62497138a4
Andrii Blacksmith <coderpy4@proton.me>
2024-05-05 21:56:41 +0300

    fix(collections): add y-padding for collections

--- app/(pages)/collections/(collections)/components/ui/collection-item.tsx
+++ app/(pages)/collections/(collections)/components/ui/collection-item.tsx
@@ -89,7 +89,7 @@ const CollectionItem: FC<Props> = ({ collection }) => {
                     ))}
                 </div>
             )}
-            <Stack size={6} className="grid-min-10">
+            <Stack size={6} className="grid-min-10 -inset-y-4 py-4">
                 {collection.collection.map((item) => (
                     <EntryCard
                         containerClassName={cn(
