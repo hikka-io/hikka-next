76428c62c0a6a4ad4b2be755c2969b19ebf0a38e
olexh <typicaladmsky@gmail.com>
2024-05-07 02:09:45 +0300

    feature: increase cookie Max-age to 30 days

--- utils/cookies.ts
+++ utils/cookies.ts
@@ -4,7 +4,7 @@ import { cookies } from 'next/headers';
 
 export async function setCookie(name: string, value: string) {
     cookies().set(name, value, {
-        maxAge: 60 * 60 * 24 * 7,
+        maxAge: 30 * 24 * 60 * 60,
         httpOnly: true,
         sameSite: 'lax',
     });
