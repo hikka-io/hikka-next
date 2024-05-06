e613e4b9f6375d87cb6908e7038669c2eac3d69f
olexh <typicaladmsky@gmail.com>
2024-05-06 15:34:31 +0300

    feature: add user history page, improve history item, move user pages to (user) route group

--- app/(pages)/(root)/components/history.tsx
+++ app/(pages)/(root)/components/history.tsx
@@ -2,29 +2,33 @@
 
 import { FC } from 'react';
 
+import HistoryItem from '@/components/history-item';
 import Block from '@/components/ui/block';
 import Header from '@/components/ui/header';
 import NotFound from '@/components/ui/not-found';
+import useSession from '@/services/hooks/auth/useSession';
 import useUserHistory from '@/services/hooks/history/useFollowingHistory';
 import { cn } from '@/utils/utils';
-
-import HistoryItem from './components/ui/history-item';
 
 interface Props {
     className?: string;
 }
 
 const History: FC<Props> = ({ className }) => {
+    const { user } = useSession();
     const { list } = useUserHistory();
 
     const filteredHistory = list?.slice(0, 3);
 
     return (
         <Block className={cn(className)}>
-            <Header title="Активність" />
+            <Header
+                title="Історія"
+                href={`/u/${user?.username}/history?type=following`}
+            />
             <div className="flex flex-col gap-6">
                 {filteredHistory?.map((item) => (
-                    <HistoryItem data={item} key={item.reference} />
+                    <HistoryItem data={item} key={item.reference} withUser />
                 ))}
                 {filteredHistory?.length === 0 && (
                     <NotFound
--- app/(pages)/(root)/page.tsx
+++ app/(pages)/(root)/page.tsx
@@ -4,25 +4,19 @@ import { dehydrate } from '@tanstack/query-core';
 import { HydrationBoundary } from '@tanstack/react-query';
 
 import Ongoings from '@/app/(pages)/(root)/components/ongoings';
+import prefetchQueries from '@/app/(pages)/(root)/page.queries';
 import UserCover from '@/components/user-cover';
-import getFollowingHistory from '@/services/api/history/getFollowingHistory';
-import getAnimeSchedule from '@/services/api/stats/getAnimeSchedule';
 import getLoggedUserInfo from '@/services/api/user/getLoggedUserInfo';
-import getWatchList from '@/services/api/watch/getWatchList';
-import getCurrentSeason from '@/utils/getCurrentSeason';
 import getQueryClient from '@/utils/getQueryClient';
 
 import Collections from './components/collections';
 import Comments from './components/comments';
-import History from './components/history/history';
+import History from './components/history';
 import Profile from './components/profile';
 import Schedule from './components/schedule/schedule';
-import getCollections from '@/services/api/collections/getCollections';
-import getLatestComments from '@/services/api/comments/getLatestComments';
 
 
 const Page = async () => {
-    const season = getCurrentSeason()!;
     const queryClient = await getQueryClient();
 
     await queryClient.prefetchQuery({
@@ -34,81 +28,7 @@ const Page = async () => {
         'loggedUser',
     ]);
 
-    if (loggedUser) {
-        await queryClient.prefetchInfiniteQuery({
-            initialPageParam: 1,
-            queryKey: [
-                'watchList',
-                loggedUser.username,
-                {
-                    ageRatings: [],
-                    genres: [],
-                    order: 'desc',
-                    seasons: [],
-                    sort: 'watch_score',
-                    statuses: [],
-                    types: [],
-                    watch_status: 'watching',
-                    years: [],
-                },
-            ],
-            queryFn: ({ pageParam = 1, meta }) =>
-                getWatchList({
-                    params: {
-                        username: loggedUser.username,
-                        genres: [],
-                        media_type: [],
-                        rating: [],
-                        season: [],
-                        sort: ['watch_score:desc'],
-                        status: [],
-                        watch_status: 'watching',
-                    },
-                    page: pageParam,
-                    auth: meta?.auth,
-                }),
-        });
-
-        await queryClient.prefetchInfiniteQuery({
-            initialPageParam: 1,
-            queryKey: ['followingHistory'],
-            queryFn: ({ pageParam, meta }) =>
-                getFollowingHistory({
-                    page: pageParam,
-                    auth: meta?.auth,
-                }),
-        });
-    }
-
-    await queryClient.prefetchInfiniteQuery({
-        initialPageParam: 1,
-        queryKey: [
-            'animeSchedule',
-            {
-                season,
-                status: ['ongoing', 'announced'],
-                year: String(new Date().getFullYear()),
-            },
-        ],
-        queryFn: ({ pageParam = 1 }) =>
-            getAnimeSchedule({
-                params: {
-                    airing_season: [season, String(new Date().getFullYear())],
-                    status: ['ongoing', 'announced'],
-                },
-                page: pageParam,
-            }),
-    });
-
-    await queryClient.prefetchQuery({
-        queryKey: ['collections', { page: 1, sort: 'created' }],
-        queryFn: () => getCollections({ page: 1, params: { sort: 'created' } }),
-    })
-
-    await queryClient.prefetchQuery({
-        queryKey: ['latestComments'],
-        queryFn: () => getLatestComments(),
-    })
+    await prefetchQueries({ queryClient });
 
     const dehydratedState = dehydrate(queryClient);
 
