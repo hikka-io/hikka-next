8c0f79a9c0c8128815c7159d5803427e40079ee2
olexh <typicaladmsky@gmail.com>
2024-05-08 23:34:57 +0300

    fix(session-manager): prevent loggedUser fetch when auth cookie is missing

--- app/(pages)/components/session-manager.tsx
+++ app/(pages)/components/session-manager.tsx
@@ -4,18 +4,21 @@ import { dehydrate } from '@tanstack/query-core';
 import { HydrationBoundary } from '@tanstack/react-query';
 
 import getLoggedUserInfo from '@/services/api/user/getLoggedUserInfo';
+import { getCookie } from '@/utils/cookies';
 import getQueryClient from '@/utils/getQueryClient';
 
 interface Props extends PropsWithChildren {}
 
 const SessionManager = async ({ children }: Props) => {
     const queryClient = await getQueryClient();
+    const auth = await getCookie('auth');
 
     try {
-        await queryClient.fetchQuery({
-            queryKey: ['loggedUser'],
-            queryFn: ({ meta }) => getLoggedUserInfo({ auth: meta?.auth }),
-        });
+        auth &&
+            (await queryClient.fetchQuery({
+                queryKey: ['loggedUser'],
+                queryFn: ({ meta }) => getLoggedUserInfo({ auth: meta?.auth }),
+            }));
     } catch (e) {
         console.log(e);
         // await deleteCookie('auth');
