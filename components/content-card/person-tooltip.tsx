4deaa17fc25e82ccb702764da9310c73232f9e80
olexh <typicaladmsky@gmail.com>
2024-06-10 20:13:50 +0300

    feat: improve person tooltip

--- components/content-card/person-tooltip.tsx
+++ components/content-card/person-tooltip.tsx
@@ -14,7 +14,10 @@ import {
 } from '@/components/ui/hover-card';
 import { Label } from '@/components/ui/label';
 
+import { PersonAnime } from '@/services/api/people/getPersonAnime';
+import { PersonCharacter } from '@/services/api/people/getPersonCharacters';
 import usePersonAnime from '@/services/hooks/people/use-person-anime';
+import usePersonCharacters from '@/services/hooks/people/use-person-characters';
 import usePersonInfo from '@/services/hooks/people/use-person-info';
 
 interface TooltipDataProps {
@@ -26,11 +29,97 @@ interface Props extends PropsWithChildren {
     withTrigger?: boolean;
 }
 
+const PersonAnimeList: FC<{ list?: PersonAnime[]; slug: string }> = ({
+    list,
+    slug,
+}) => {
+    if (!list || list.length === 0) return null;
+
+    return (
+        <div className="flex flex-col gap-2">
+            <Label className="text-muted-foreground">Роботи</Label>
+
+            <div className="flex gap-2">
+                {list.slice(0, 5).map(({ anime }) => (
+                    <ContentCard
+                        className="w-10"
+                        href={`/anime/${anime.slug}`}
+                        key={anime.slug}
+                        poster={anime.poster}
+                        slug={anime.slug}
+                        content_type={anime.data_type}
+                        containerRatio={0.7}
+                    />
+                ))}
+                {list.length > 5 && (
+                    <ContentCard
+                        className="w-10"
+                        href={`/people/${slug}`}
+                        poster={
+                            <MaterialSymbolsMoreHoriz className="text-4xl text-muted-foreground" />
+                        }
+                        containerRatio={0.7}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
+const PersonCharactersList: FC<{ list?: PersonCharacter[]; slug: string }> = ({
+    list,
+    slug,
+}) => {
+    if (!list || list.length === 0) return null;
+
+    return (
+        <div className="flex flex-col gap-2">
+            <Label className="text-muted-foreground">Ролі</Label>
+
+            <div className="flex gap-2">
+                {list.slice(0, 5).map(({ character }) => (
+                    <ContentCard
+                        className="w-10"
+                        href={`/characters/${character.slug}`}
+                        key={character.slug}
+                        poster={character.image}
+                        slug={character.slug}
+                        content_type={character.data_type}
+                        containerRatio={0.7}
+                    />
+                ))}
+                {list.length > 5 && (
+                    <ContentCard
+                        className="w-10"
+                        href={`/people/${slug}`}
+                        poster={
+                            <MaterialSymbolsMoreHoriz className="text-4xl text-muted-foreground" />
+                        }
+                        containerRatio={0.7}
+                    />
+                )}
+            </div>
+        </div>
+    );
+};
+
 const TooltipData: FC<TooltipDataProps> = ({ slug }) => {
     const { data } = usePersonInfo({ slug });
-    const { list } = usePersonAnime({ slug });
+    const { list: anime } = usePersonAnime(
+        { slug },
+        {
+            enabled:
+                data && data.characters_count === 0 && data.anime_count > 0,
+        },
+    );
+    const { list: characters } = usePersonCharacters(
+        { slug },
+        {
+            enabled: data && data.characters_count > 0,
+        },
+    );
 
-    if (!data || !list) {
+    if (!data || (!anime && !characters)) {
         return (
             <div className="flex w-96 animate-pulse gap-4 text-left">
                 <div className="h-28 w-20 rounded-lg bg-secondary/60" />
@@ -38,9 +127,6 @@ const TooltipData: FC<TooltipDataProps> = ({ slug }) => {
                     <div className="flex flex-col gap-2">
                         <div className="flex w-full flex-1 flex-col gap-2">
                             <div className="h-5 w-20 rounded-lg bg-secondary/60" />
-                            <div className="h-2 w-full rounded-lg bg-secondary/60" />
-                            <div className="h-2 w-full rounded-lg bg-secondary/60" />
-                            <div className="h-2 w-full rounded-lg bg-secondary/60" />
                         </div>
                     </div>
 
@@ -80,33 +166,10 @@ const TooltipData: FC<TooltipDataProps> = ({ slug }) => {
                         {data.description_ua}
                     </MDViewer>
 
-                    <div>
-                        <Label className="text-muted-foreground">Роботи</Label>
-                    </div>
-
-                    <div className="flex gap-2">
-                        {list.slice(0, 5).map(({ anime }) => (
-                            <ContentCard
-                                className="w-10"
-                                href={`/anime/${anime.slug}`}
-                                key={anime.slug}
-                                poster={anime.poster}
-                                slug={anime.slug}
-                                content_type={anime.data_type}
-                                containerRatio={0.7}
-                            />
-                        ))}
-                        {list.length > 5 && (
-                            <ContentCard
-                                className="w-10"
-                                href={`/people/${data.slug}`}
-                                poster={
-                                    <MaterialSymbolsMoreHoriz className="text-4xl text-muted-foreground" />
-                                }
-                                containerRatio={0.7}
-                            />
-                        )}
-                    </div>
+                    {data.characters_count === 0 && (
+                        <PersonAnimeList list={anime} slug={data.slug} />
+                    )}
+                    <PersonCharactersList list={characters} slug={data.slug} />
                 </div>
             </div>
         </div>
