2f1f7092ef0a374efcc039865f5319d17c993f6d
olexh <typicaladmsky@gmail.com>
2024-06-16 02:14:51 +0300

    feat: add novel-card component

---
+++ components/novel-card.tsx
@@ -0,0 +1,30 @@
+import { FC } from 'react';
+
+import ContentCard, {
+    Props as ContentCardProps,
+} from '@/components/content-card/content-card';
+
+import { NOVEL_MEDIA_TYPE } from '@/utils/constants';
+
+interface Props extends ContentCardProps {
+    novel: API.Novel | API.NovelInfo;
+}
+
+const NovelCard: FC<Props> = ({ novel, ...props }) => {
+    return (
+        <ContentCard
+            slug={novel.slug}
+            content_type="novel"
+            href={`/novel/${novel.slug}`}
+            poster={novel.image}
+            title={novel.title}
+            leftSubtitle={novel.year ? String(novel.year) : undefined}
+            rightSubtitle={
+                novel.media_type && NOVEL_MEDIA_TYPE[novel.media_type].title_ua
+            }
+            {...props}
+        />
+    );
+};
+
+export default NovelCard;
