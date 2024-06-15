7c6bb0a200b20ec4be6a5eaf5a8ce871b4b0816e
olexh <typicaladmsky@gmail.com>
2024-06-16 00:04:36 +0300

    feat: add readlist-button component

---
+++ components/readlist-button/readlist-button.tsx
@@ -0,0 +1,175 @@
+'use client';
+
+import { createElement } from 'react';
+import MaterialSymbolsSettingsOutline from '~icons/material-symbols/settings-outline';
+
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectList,
+    SelectSeparator,
+} from '@/components/ui/select';
+
+import ReadEditModal from '@/features/modals/read-edit-modal';
+
+import useMangaInfo from '@/services/hooks/manga/use-manga-info';
+import useAddRead from '@/services/hooks/read/use-add-read';
+import useRead from '@/services/hooks/read/use-read';
+import { useModalContext } from '@/services/providers/modal-provider';
+import { READ_STATUS } from '@/utils/constants';
+
+import NewStatusTrigger from './new-status-trigger';
+import ReadStatusTrigger from './read-status-trigger';
+
+interface Props {
+    slug: string;
+    additional?: boolean;
+    disabled?: boolean;
+    content_type: 'novel' | 'manga';
+}
+
+const SETTINGS_BUTTON = {
+    label: (
+        <div className="flex items-center gap-2">
+            <MaterialSymbolsSettingsOutline />
+            Налаштування
+        </div>
+    ),
+    value: 'settings',
+    disableCheckbox: true,
+    title: 'Налаштування',
+};
+
+const OPTIONS = [
+    ...Object.keys(READ_STATUS).map((status) => ({
+        value: status,
+        title: READ_STATUS[status as API.ReadStatus].title_ua,
+        label: (
+            <div className="flex items-center gap-2">
+                {createElement(READ_STATUS[status as API.ReadStatus].icon!)}
+                {READ_STATUS[status as API.ReadStatus].title_ua}
+            </div>
+        ),
+    })),
+];
+
+const Component = ({ slug, content_type, disabled }: Props) => {
+    const { openModal } = useModalContext();
+
+    const { data: read, isError: readError } = useRead(
+        {
+            slug,
+            content_type,
+        },
+        { enabled: !disabled },
+    );
+    const { data: manga } = useMangaInfo(
+        {
+            slug,
+        },
+        { enabled: !disabled && content_type === 'manga' },
+    );
+    const { mutate: addRead } = useAddRead();
+
+    const openReadEditModal = () => {
+        if (manga) {
+            openModal({
+                content: (
+                    <ReadEditModal
+                        content_type={content_type}
+                        slug={manga.slug}
+                    />
+                ),
+                className: '!max-w-xl',
+                title: manga.title,
+                forceModal: true,
+            });
+        }
+    };
+
+    const handleChangeStatus = (options: string[]) => {
+        const params =
+            read && !readError
+                ? {
+                      volumes: read?.volumes || undefined,
+                      score: read?.score || undefined,
+                      note: read?.note || undefined,
+                      rereads: read?.rereads || undefined,
+                  }
+                : {};
+
+        if (options[0] === 'settings') {
+            openReadEditModal();
+            return;
+        }
+
+        if (options[0] === 'completed') {
+            addRead({
+                params: {
+                    slug,
+                    content_type,
+                    status: 'completed',
+                    ...params,
+                    chapters: manga?.chapters,
+                },
+            });
+        } else {
+            addRead({
+                params: {
+                    slug,
+                    content_type,
+                    status: options[0] as API.ReadStatus,
+                    ...params,
+                },
+            });
+        }
+    };
+
+    return (
+        <Select
+            value={read && !readError ? [read.status] : []}
+            onValueChange={handleChangeStatus}
+        >
+            {read && !readError ? (
+                <ReadStatusTrigger
+                    content_type={content_type}
+                    read={read!}
+                    disabled={disabled}
+                    slug={slug}
+                />
+            ) : (
+                <NewStatusTrigger
+                    content_type={content_type}
+                    slug={slug}
+                    disabled={disabled}
+                />
+            )}
+
+            <SelectContent>
+                <SelectList>
+                    <SelectGroup>
+                        {OPTIONS.map((option) => (
+                            <SelectItem key={option.value} value={option.value}>
+                                {option.label}
+                            </SelectItem>
+                        ))}
+                    </SelectGroup>
+                    {read && !readError && <SelectSeparator />}
+                    {read && !readError && (
+                        <SelectGroup>
+                            {read && !readError && (
+                                <SelectItem disableCheckbox value="settings">
+                                    {SETTINGS_BUTTON.label}
+                                </SelectItem>
+                            )}
+                        </SelectGroup>
+                    )}
+                </SelectList>
+            </SelectContent>
+        </Select>
+    );
+};
+
+export default Component;
