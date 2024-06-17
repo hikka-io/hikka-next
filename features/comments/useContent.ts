e932d79d08dbb41782be34d9e3304e36aa97406f
olexh <typicaladmsky@gmail.com>
2024-06-17 15:58:43 +0300

    feat: add manga and novel support for use-content hook

--- features/comments/useContent.ts
+++ features/comments/useContent.ts
@@ -4,6 +4,8 @@ import getAnimeInfo from '@/services/api/anime/getAnimeInfo';
 import getCharacterInfo from '@/services/api/characters/getCharacterInfo';
 import getCollection from '@/services/api/collections/getCollection';
 import getEdit from '@/services/api/edit/getEdit';
+import getMangaInfo from '@/services/api/manga/getMangaInfo';
+import getNovelInfo from '@/services/api/novel/getNovelInfo';
 import getPersonInfo from '@/services/api/people/getPersonInfo';
 import { useSettingsContext } from '@/services/providers/settings-provider';
 
@@ -22,6 +24,10 @@ export const getContent = ({ content_type, slug }: Props) => {
     switch (content_type) {
         case 'anime':
             return getAnimeInfo({ params: { slug } });
+        case 'manga':
+            return getMangaInfo({ params: { slug } });
+        case 'novel':
+            return getNovelInfo({ params: { slug } });
         case 'character':
             return getCharacterInfo({ params: { slug } });
         case 'person':
@@ -53,6 +59,21 @@ const useContent = ({ slug, content_type }: Props) => {
                             data.title_en ||
                             data.title_ja,
                         image: data.poster,
+                        content_type,
+                    };
+                }
+
+                if (data.data_type === 'manga' || data.data_type === 'novel') {
+                    content = {
+                        title:
+                            data[
+                                titleLanguage === 'title_ja'
+                                    ? 'title_original'
+                                    : titleLanguage!
+                            ] ||
+                            data.title_ua ||
+                            data.title_en,
+                        image: data.image,
                         content_type,
                     };
                 }
