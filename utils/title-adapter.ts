456ed6c559256f1f322d606cffd2dd21418103ae
olexh <typicaladmsky@gmail.com>
2024-06-17 15:57:12 +0300

    feat: add getTitle

--- utils/title-adapter.ts
+++ utils/title-adapter.ts
@@ -3,14 +3,20 @@ type TitleData = {
     title_ua?: string;
     title_ja?: string;
     title_original?: string;
+    name_ua?: string;
+    name_en?: string;
+    name_ja?: string;
+    name_native?: string;
 };
+
+type TitleLanguage = keyof TitleData;
 
 export const convertTitle = <TData>({
     data,
     titleLanguage,
 }: {
     data: TData & TitleData;
-    titleLanguage: 'title_en' | 'title_ua' | 'title_ja';
+    titleLanguage: TitleLanguage;
 }) => {
     return {
         ...data,
@@ -22,7 +28,12 @@ export const convertTitle = <TData>({
             data.title_ua ||
             data.title_en ||
             data.title_ja ||
-            data.title_original,
+            data.title_original ||
+            data.name_ua ||
+            data.name_en ||
+            data.name_ja ||
+            data.name_native ||
+            '',
     };
 };
 
@@ -31,7 +42,17 @@ export const convertTitleList = <TData>({
     titleLanguage,
 }: {
     data: (TData & TitleData)[];
-    titleLanguage: 'title_en' | 'title_ua' | 'title_ja';
+    titleLanguage: TitleLanguage;
 }) => {
     return data.map((entry) => convertTitle({ titleLanguage, data: entry }));
+};
+
+export const getTitle = <TData>({
+    data,
+    titleLanguage,
+}: {
+    data: TData & TitleData;
+    titleLanguage: TitleLanguage;
+}) => {
+    return convertTitle({ data, titleLanguage }).title;
 };
