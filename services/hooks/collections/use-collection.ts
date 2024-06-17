0d872eab26f687d80ba9fdccd391c5995e60d906
olexh <typicaladmsky@gmail.com>
2024-06-17 15:57:50 +0300

    feat: convert titles in hooks

--- services/hooks/collections/use-collection.ts
+++ services/hooks/collections/use-collection.ts
@@ -3,11 +3,27 @@ import { useQuery } from '@tanstack/react-query';
 import getCollection, {
     Params,
 } from '@/services/api/collections/getCollection';
+import { useSettingsContext } from '@/services/providers/settings-provider';
+import { convertTitle } from '@/utils/title-adapter';
 
 const useCollection = ({ reference }: Params, options?: Hikka.QueryOptions) => {
+    const { titleLanguage } = useSettingsContext();
+
     return useQuery({
         queryKey: ['collection', reference],
         queryFn: () => getCollection({ params: { reference } }),
+        select: (data) => ({
+            ...data,
+            collection: data.collection.map((c) => ({
+                ...c,
+                content: {
+                    ...convertTitle({
+                        data: c.content,
+                        titleLanguage: titleLanguage!,
+                    }),
+                },
+            })),
+        }),
         ...options,
     });
 };
--- services/hooks/collections/use-collections.ts
+++ services/hooks/collections/use-collections.ts
@@ -3,14 +3,31 @@ import { useQuery } from '@tanstack/react-query';
 import getCollections, {
     Params,
 } from '@/services/api/collections/getCollections';
+import { useSettingsContext } from '@/services/providers/settings-provider';
+import { convertTitle } from '@/utils/title-adapter';
 
 const useCollections = (
     { page, sort = 'system_ranking' }: Params,
     options?: Hikka.QueryOptions,
 ) => {
+    const { titleLanguage } = useSettingsContext();
+
     return useQuery({
         queryKey: ['collections', { page, sort }],
         queryFn: () => getCollections({ page, params: { sort } }),
+        select: (data) => ({
+            pagination: data.pagination,
+            list: data.list.map((l) => ({
+                ...l,
+                collection: l.collection.map((c) => ({
+                    ...c,
+                    content: convertTitle({
+                        data: c.content,
+                        titleLanguage: titleLanguage!,
+                    }),
+                })),
+            })),
+        }),
         ...options,
     });
 };
