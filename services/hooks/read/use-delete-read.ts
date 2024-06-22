d9a0b1199d962fd418b0c1b57e4e29089615049c
olexh <typicaladmsky@gmail.com>
2024-06-22 18:07:13 +0300

    fix: make global invalidation of queries

--- services/hooks/read/use-delete-read.ts
+++ services/hooks/read/use-delete-read.ts
@@ -9,23 +9,7 @@ const useDeleteRead = () => {
         mutationKey: ['delete-from-read-list'],
         mutationFn: deleteRead,
         onSuccess: async () => {
-            await queryClient.refetchQueries({
-                queryKey: ['read'],
-                exact: false,
-            });
-            await queryClient.invalidateQueries({ queryKey: ['novel-list'] });
-            await queryClient.invalidateQueries({ queryKey: ['manga-list'] });
-            await queryClient.invalidateQueries({
-                queryKey: ['read-list'],
-                exact: false,
-            });
-            await queryClient.invalidateQueries({ queryKey: ['favorites'] });
-            await queryClient.invalidateQueries({ queryKey: ['franchise'] });
-            await queryClient.invalidateQueries({ queryKey: ['collection'] });
-            await queryClient.invalidateQueries({
-                queryKey: ['anime-schedule', {}],
-                exact: false,
-            });
+            await queryClient.refetchQueries();
         },
     });
 };
