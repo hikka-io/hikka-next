31dee05cc6ce3b7d8d2781dec1e87298b9c5c5dd
olexh <typicaladmsky@gmail.com>
2024-06-24 01:08:30 +0300

    feat: add sitemaps for anime, manga and novel

--- services/api/sitemap/getMangaSitemap.ts
+++ services/api/sitemap/getMangaSitemap.ts
@@ -9,7 +9,7 @@ export default async function req(
     props?: BaseFetchRequestProps,
 ): Promise<Response[]> {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/sitemap/sitemap_anime.json`,
+        `${process.env.NEXT_PUBLIC_API_URL}/sitemap/sitemap_manga.json`,
         {
             method: 'get',
             headers: {
--- services/api/sitemap/getNovelSitemap.ts
+++ services/api/sitemap/getNovelSitemap.ts
@@ -9,7 +9,7 @@ export default async function req(
     props?: BaseFetchRequestProps,
 ): Promise<Response[]> {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/sitemap/sitemap_anime.json`,
+        `${process.env.NEXT_PUBLIC_API_URL}/sitemap/sitemap_novel.json`,
         {
             method: 'get',
             headers: {
