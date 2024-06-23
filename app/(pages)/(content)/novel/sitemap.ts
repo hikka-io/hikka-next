d5363e284948cc9f7b8b9bc997ab7ead1e886918
olexh <typicaladmsky@gmail.com>
2024-06-24 01:08:30 +0300

    feat: add sitemaps for anime, manga and novel

---
+++ app/(pages)/(content)/novel/sitemap.ts
@@ -0,0 +1,17 @@
+import toDate from 'date-fns/toDate';
+import { MetadataRoute } from 'next';
+
+import getNovelSitemap from '@/services/api/sitemap/getNovelSitemap';
+
+export const dynamic = 'force-dynamic';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const res = await getNovelSitemap();
+
+    return res.map((novel) => ({
+        url: 'https://hikka.io/novel/' + novel.slug,
+        lastModified: toDate(novel.updated_at * 1000),
+        changeFrequency: 'weekly',
+        priority: 1,
+    }));
+}
