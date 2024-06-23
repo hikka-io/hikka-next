8f7e1e88ab9d495246518850f35f6b14e15a29cc
olexh <typicaladmsky@gmail.com>
2024-06-23 21:52:13 +0300

    fix: make api redirects public-only

--- next.config.mjs
+++ next.config.mjs
@@ -33,7 +33,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.API_URL}/:path*`,
+                destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
             },
         ];
     },
