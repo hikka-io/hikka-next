cf025e03f365501eea31219ef666eac98e98e535
Lorg0n <lorgon.kv@gmail.com>
2024-07-09 17:58:10 +0300

    feat: create warning components

--- app/(pages)/(content)/novel/[slug]/page.tsx
+++ app/(pages)/(content)/novel/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 import Characters from '@/features/novel/novel-view/characters/characters.component';
 import Description from '@/features/novel/novel-view/description.component';
 import Details from '@/features/novel/novel-view/details/details.component';
+import Warning from '@/features/novel/novel-view/details/warning.component';
 import Franchise from '@/features/novel/novel-view/franchise.component';
 import Links from '@/features/novel/novel-view/links/links.component';
 import ReadStats from '@/features/novel/novel-view/read-stats/read-stats.component';
@@ -18,6 +19,7 @@ const NovelPage: FC<Props> = async ({ params }) => {
     return (
         <div className="grid grid-cols-1 gap-12 lg:grid-cols-[1fr_33%] lg:gap-16 xl:grid-cols-[1fr_30%]">
             <div className="relative order-2 flex flex-col gap-12 lg:order-1">
+                <Warning />
                 <Description />
                 <Characters />
                 <Franchise />
---
+++ features/novel/novel-view/details/warning.component.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { useParams } from 'next/navigation';
+
+import P from '@/components/typography/p';
+import Block from '@/components/ui/block';
+import Card from '@/components/ui/card';
+
+import { WARNING_WORDS } from '@/utils/constants';
+
+import useAnimeInfo from '@/services/hooks/anime/use-anime-info';
+
+const Warning = () => {
+    const params = useParams();
+    const { data } = useAnimeInfo({ slug: String(params.slug) });
+
+    const containsWord = (texts: (string | undefined)[], words: string[]): boolean => {
+        return texts.some(text =>
+            words.some(word =>
+                text?.toLowerCase().includes(word.toLowerCase())
+            )
+        );
+    };
+
+    if (!data || !containsWord([data.title_en, data.title_ja, data.title, data.synopsis_en], WARNING_WORDS)) {
+        return null;
+    }
+
+    return (
+        <Block>
+            <Card className='border-destructive/60'>
+                <P className="text-sm text-muted-foreground">
+                    {"Примітка модерації"}
+                </P>
+                <P className='font-semibold text-destructive'>Цей твір просуває російську культуру та не рекомендований до перегляду чи читання.</P>
+            </Card>
+        </Block>
+    );
+};
+
+export default Warning;
