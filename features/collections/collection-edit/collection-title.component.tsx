24336239641184bf0f13076290f7251e016db3b9
Lorg0n <lorgon.kv@gmail.com>
2024-07-13 23:26:44 +0300

    feat: add collection rules alert

--- features/collections/collection-edit/collection-title.component.tsx
+++ features/collections/collection-edit/collection-title.component.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'next/navigation';
 
 import PlateEditor from '@/components/markdown/plate-editor/plate-editor';
 import Header from '@/components/ui/header';
+import RulesAlert from '@/features/collections/collection-edit/collection-rules-alert.component';
 
 import { useCollectionContext } from '@/services/providers/collection-provider';
 
@@ -19,6 +20,7 @@ const CollectionTitle = () => {
     return (
         <div className="flex flex-col gap-4">
             <Header title={title || 'Нова колекція'} />
+            <RulesAlert />
             {((reference && description !== undefined) || !reference) && (
                 <PlateEditor
                     onChange={(markdown) =>
