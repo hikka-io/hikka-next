320d4c3a2e6aed3e0bc0d183ce8c27232d9dd7bf
olexh <typicaladmsky@gmail.com>
2024-08-24 02:56:40 +0300

    feat: add prefetching clients

--- src/app/(pages)/(user)/settings/(categories)/applications/page.tsx
+++ src/app/(pages)/(user)/settings/(categories)/applications/page.tsx
@@ -1,9 +1,14 @@
+import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
 import { FC } from 'react';
 
+import P from '@/components/typography/p';
 import Header from '@/components/ui/header';
 
 import Applications from '@/features/settings/applications/applications';
 import ClientCreateButton from '@/features/settings/applications/client-create-button';
+
+import { prefetchClients } from '@/services/hooks/client/use-clients';
+import getQueryClient from '@/utils/get-query-client';
 
 interface Props {
     params: {
@@ -12,15 +17,27 @@ interface Props {
 }
 
 const ApplicationsSettingsPage: FC<Props> = async ({ params }) => {
+    const queryClient = getQueryClient();
+
+    await prefetchClients();
+
+    const dehydratedState = dehydrate(queryClient);
+
     return (
-        <div className="flex flex-col gap-8">
-            <div className="flex flex-col">
-                <Header title="Сторонні додатки">
-                    <ClientCreateButton />
-                </Header>
+        <HydrationBoundary state={dehydratedState}>
+            <div className="flex flex-col gap-8">
+                <div className="flex flex-col">
+                    <Header title="Застосунки">
+                        <ClientCreateButton />
+                    </Header>
+                    <P className="text-sm text-muted-foreground">
+                        Підключіть OAuth авторизацію через hikka за допомогою
+                        застосунку (для розробників)
+                    </P>
+                </div>
+                <Applications />
             </div>
-            <Applications />
-        </div>
+        </HydrationBoundary>
     );
 };
 
