be69de85e83bfb607d283f8edb1a94eb90916395
olexh <typicaladmsky@gmail.com>
2024-08-24 03:50:46 +0300

    fix: replace query by mutation

--- src/services/hooks/auth/use-request-token-reference.ts
+++ src/services/hooks/auth/use-request-token-reference.ts
@@ -1,47 +1,12 @@
-import { QueryKey, useQuery } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 
-import requestTokenReference, {
-    Params,
-} from '@/services/api/auth/requestTokenReference';
-import getQueryClient from '@/utils/get-query-client';
+import requestTokenReference from '@/services/api/auth/requestTokenReference';
 
-export const paramsBuilder = (props: Params): Params => ({
-    client_reference: props.client_reference,
-    scope: props.scope,
-});
-
-export const key = (params: Params): QueryKey => [
-    'token-reference',
-    params.client_reference,
-];
-
-const useRequestTokenReference = (
-    props: Params,
-    options?: Hikka.QueryOptions,
-) => {
-    const params = paramsBuilder(props);
-
-    return useQuery({
-        queryKey: key(params),
-        queryFn: () =>
-            requestTokenReference({
-                params,
-            }),
-        ...options,
-        refetchOnWindowFocus: false,
-    });
-};
-
-export const prefetchRequestTokenReference = (props: Params) => {
-    const queryClient = getQueryClient();
-    const params = paramsBuilder(props);
-
-    return queryClient.prefetchQuery({
-        queryKey: key(params),
-        queryFn: () =>
-            requestTokenReference({
-                params,
-            }),
+const useRequestTokenReference = () => {
+    return useMutation({
+        mutationKey: ['token-reference'],
+        mutationFn: requestTokenReference,
+        onSuccess: () => {},
     });
 };
 
