dc042ace271bf47a87ddc9930f92dd301584daaa
olexh <typicaladmsky@gmail.com>
2024-12-17 16:31:48 +0200

    fix: correct params in getArticles

--- src/services/api/articles/getArticles.tsx
+++ src/services/api/articles/getArticles.tsx
@@ -6,18 +6,25 @@ import {
 export interface Response extends API.WithPagination<API.Article> {}
 
 export interface Params {
-    slug: string;
     page?: number;
     size?: number;
-    sort?: string;
+    sort?: string[];
     author?: string;
     draft?: boolean;
 }
 
 export default async function req({
+    params,
     ...props
 }: BaseFetchRequestProps<Params>): Promise<Response> {
     return fetchRequest<Response>({
+        params: {
+            author: params?.author,
+            draft: params?.draft,
+            sort: params?.sort,
+        },
+        page: params?.page,
+        size: params?.size,
         ...props,
         path: `/articles`,
         method: 'post',
