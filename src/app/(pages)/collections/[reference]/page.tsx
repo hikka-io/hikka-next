2bc1ec558dcc513bf503df0f3972721a66e4e02e
olexh <typicaladmsky@gmail.com>
2024-12-18 19:10:52 +0200

    refactor: remove redundant wrapper

--- src/app/(pages)/collections/[reference]/page.tsx
+++ src/app/(pages)/collections/[reference]/page.tsx
@@ -67,32 +67,30 @@ const CollectionPage = async (props: {
     return (
         <HydrationBoundary state={dehydratedState}>
             <CollectionProvider>
-                <>
-                    <Breadcrumbs>
-                        <div className="flex w-auto items-center gap-4 overflow-hidden whitespace-nowrap">
-                            <Link
-                                href={'/collections/' + reference}
-                                className="flex-1 overflow-hidden text-ellipsis text-sm font-bold hover:underline"
-                            >
-                                {collection?.title}
-                            </Link>
-                        </div>
-                    </Breadcrumbs>
-                    <div className="grid grid-cols-1 justify-center lg:grid-cols-[1fr_25%] lg:items-start lg:justify-between lg:gap-16">
-                        <div className="flex flex-col gap-12">
-                            <Block>
-                                <CollectionTitle />
-                                <div className="block lg:hidden">
-                                    <CollectionInfo />
-                                </div>
-                                <CollectionGroups />
-                            </Block>
-                        </div>
-                        <div className="order-1 hidden w-full lg:order-2 lg:flex lg:h-full lg:flex-col">
-                            <CollectionInfo />
-                        </div>
+                <Breadcrumbs>
+                    <div className="flex w-auto items-center gap-4 overflow-hidden whitespace-nowrap">
+                        <Link
+                            href={'/collections/' + reference}
+                            className="flex-1 overflow-hidden text-ellipsis text-sm font-bold hover:underline"
+                        >
+                            {collection?.title}
+                        </Link>
                     </div>
-                </>
+                </Breadcrumbs>
+                <div className="grid grid-cols-1 justify-center lg:grid-cols-[1fr_25%] lg:items-start lg:justify-between lg:gap-16">
+                    <div className="flex flex-col gap-12">
+                        <Block>
+                            <CollectionTitle />
+                            <div className="block lg:hidden">
+                                <CollectionInfo />
+                            </div>
+                            <CollectionGroups />
+                        </Block>
+                    </div>
+                    <div className="order-1 hidden w-full lg:order-2 lg:flex lg:h-full lg:flex-col">
+                        <CollectionInfo />
+                    </div>
+                </div>
             </CollectionProvider>
         </HydrationBoundary>
     );
