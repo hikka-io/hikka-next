f76fd2a7e86d44b3024f50ee1896ccc94587587a
olexh <typicaladmsky@gmail.com>
2024-12-29 16:05:14 +0200

    fix: logout user(last hope)

--- src/app/(api)/auth/logout/route.ts
+++ src/app/(api)/auth/logout/route.ts
@@ -6,6 +6,13 @@ export async function GET() {
     response.cookies.set('auth', '', {
         path: '/',
         expires: new Date(0),
+        domain: process.env.COOKIE_DOMAIN,
+        httpOnly: process.env.COOKIE_HTTP_ONLY === 'true',
+    });
+
+    response.cookies.set('auth', '', {
+        path: '/',
+        expires: new Date(0),
     });
 
     return response;
