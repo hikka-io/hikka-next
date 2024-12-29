03288bd9d4097c90f452cb32b64b6b4b675a838e
olexh <typicaladmsky@gmail.com>
2024-12-29 14:51:33 +0200

    fix: logout user

--- src/app/(api)/auth/logout/route.ts
+++ src/app/(api)/auth/logout/route.ts
@@ -4,8 +4,6 @@ export async function GET() {
     (await cookies()).delete({
         name: 'auth',
         domain: process.env.COOKIE_DOMAIN,
-        httpOnly: process.env.COOKIE_HTTP_ONLY === 'true',
-        sameSite: 'lax',
     });
 
     return Response.redirect(`${process.env.SITE_URL}`);
