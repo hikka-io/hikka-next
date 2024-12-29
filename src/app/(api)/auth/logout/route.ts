1303603ef30f21b5a85a665ef9652f1162079d80
olexh <typicaladmsky@gmail.com>
2024-12-29 17:51:09 +0200

    fix: logout user(finally works)

--- src/app/(api)/auth/logout/route.ts
+++ src/app/(api)/auth/logout/route.ts
@@ -6,7 +6,6 @@ export async function GET() {
     const cookieStore = await cookies();
 
     cookieStore.getAll().forEach((cookie) => {
-        console.log(cookie);
         cookieStore.delete(cookie);
     });
 
