fa78d716ebcf05f9b9d87b54b5eac1a4b7f291ad
olexh <typicaladmsky@gmail.com>
2025-03-03 20:14:43 +0200

    feat: increase number of article tags displayed

--- src/components/article-item/article-item.tsx
+++ src/components/article-item/article-item.tsx
@@ -47,14 +47,14 @@ const ArticleItem: FC<Props> = ({ article }) => {
             </div>
             <div className="flex items-center justify-between p-4">
                 <div className="flex gap-2">
-                    {article.tags.slice(0, 1).map((tag) => (
+                    {article.tags.slice(0, 2).map((tag) => (
                         <Badge key={tag.name} variant="secondary">
                             {tag.name}
                         </Badge>
                     ))}
-                    {article.tags.length > 1 && (
+                    {article.tags.length > 2 && (
                         <Badge variant="outline">
-                            +{article.tags.length - 1}
+                            +{article.tags.length - 2}
                         </Badge>
                     )}
                 </div>
