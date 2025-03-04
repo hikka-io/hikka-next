9d7c98a4bb7db51bfba4a08b98cb07c5e3d8049a
Lorg0n <lorgon.kv@gmail.com>
2025-03-05 01:01:46 +0300

    style: unified the design for edits in all types of elements

--- src/features/people/person-view/cover.component.tsx
+++ src/features/people/person-view/cover.component.tsx
@@ -3,10 +3,14 @@
 import { useParams } from 'next/navigation';
 
 import ContentCard from '@/components/content-card/content-card';
+import EditButton from '@/components/edit-button';
+import FavoriteButton from '@/components/favorite-button';
 
+import useSession from '@/services/hooks/auth/use-session';
 import usePersonInfo from '@/services/hooks/people/use-person-info';
 
 const Cover = () => {
+    const { user: loggedUser } = useSession();
     const params = useParams();
 
     const { data: person } = usePersonInfo({ slug: String(params.slug) });
@@ -17,7 +21,23 @@ const Cover = () => {
 
     return (
         <div className="flex items-center px-16 md:px-48 lg:px-0">
-            <ContentCard image={person.image} />
+            <ContentCard image={person.image}>
+                <div className="absolute bottom-2 right-2 z-[1] flex gap-2">
+                    {loggedUser && (
+                        <EditButton
+                            key={String(params.slug)}
+                            slug={String(params.slug)}
+                            content_type="person"
+                        />
+                    )}
+                    <FavoriteButton
+                        slug={String(params.slug)}
+                        content_type="person"
+                    />
+                </div>
+
+                <div className="absolute bottom-0 left-0 h-24 w-full bg-gradient-to-t from-background to-transparent" />
+            </ContentCard>
         </div>
     );
 };
--- src/features/people/person-view/title.component.tsx
+++ src/features/people/person-view/title.component.tsx
@@ -3,7 +3,6 @@
 import { useParams } from 'next/navigation';
 import { useRef } from 'react';
 
-import EditButton from '@/components/edit-button';
 import H2 from '@/components/typography/h2';
 import P from '@/components/typography/p';
 
@@ -24,32 +23,12 @@ const Title = () => {
         <div className="flex flex-col gap-4">
             <div className="flex justify-between gap-4" ref={divRef}>
                 <div>
-                    <div className="flex gap-4">
-                        <H2>
-                            {person.name_ua ||
-                                person.name_en ||
-                                person.name_native}
-                        </H2>
-                        {loggedUser && (
-                            <EditButton
-                                key={String(params.slug)}
-                                slug={String(params.slug)}
-                                content_type="person"
-                                className="hidden lg:flex"
-                            />
-                        )}
-                    </div>
+                    <H2>
+                        {person.name_ua ||
+                            person.name_en ||
+                            person.name_native}
+                    </H2>
                     <P className="mt-2">{person.name_native}</P>
-                </div>
-                <div className="flex flex-col items-end gap-2">
-                    {loggedUser && (
-                        <EditButton
-                            key={String(params.slug)}
-                            slug={String(params.slug)}
-                            content_type="person"
-                            className="flex lg:hidden"
-                        />
-                    )}
                 </div>
             </div>
         </div>
