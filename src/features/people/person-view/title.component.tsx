60cf355534e7e91077939186c7afedd0744e7f85
olexh <typicaladmsky@gmail.com>
2025-03-05 02:31:22 +0200

    feat: move edit button to cover on person page

--- src/features/people/person-view/title.component.tsx
+++ src/features/people/person-view/title.component.tsx
@@ -3,15 +3,12 @@
 import { useParams } from 'next/navigation';
 import { useRef } from 'react';
 
-import EditButton from '@/components/edit-button';
 import H2 from '@/components/typography/h2';
 import P from '@/components/typography/p';
 
-import useSession from '@/services/hooks/auth/use-session';
 import usePersonInfo from '@/services/hooks/people/use-person-info';
 
 const Title = () => {
-    const { user: loggedUser } = useSession();
     const divRef = useRef<HTMLDivElement>(null);
     const params = useParams();
     const { data: person } = usePersonInfo({ slug: String(params.slug) });
@@ -30,26 +27,8 @@ const Title = () => {
                                 person.name_en ||
                                 person.name_native}
                         </H2>
-                        {loggedUser && (
-                            <EditButton
-                                key={String(params.slug)}
-                                slug={String(params.slug)}
-                                content_type="person"
-                                className="hidden lg:flex"
-                            />
-                        )}
                     </div>
                     <P className="mt-2">{person.name_native}</P>
-                </div>
-                <div className="flex flex-col items-end gap-2">
-                    {loggedUser && (
-                        <EditButton
-                            key={String(params.slug)}
-                            slug={String(params.slug)}
-                            content_type="person"
-                            className="flex lg:hidden"
-                        />
-                    )}
                 </div>
             </div>
         </div>
