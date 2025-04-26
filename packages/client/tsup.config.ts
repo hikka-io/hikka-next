48137cc46234775fcffad42f1267b5b214066fac
olexh <typicaladmsky@gmail.com>
2025-04-27 00:59:01 +0300

    client/feat: add tsup config

---
+++ packages/client/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    splitting: false,
+    sourcemap: true,
+    clean: true,
+    shims: true,
+    minify: true,
+    treeshake: true,
+    outExtension({ format }) {
+        return {
+            js: format === 'cjs' ? '.js' : '.mjs',
+        };
+    },
+    external: ['cross-fetch'],
+});
