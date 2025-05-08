572dbd77803805bde296a24c57a42d4d6e958468
Oleh <typicaladmsky@gmail.com>
2025-05-08 23:35:27 +0300

    refactor: disable @udecode/plate-node-id

--- src/components/markdown/editor/plugins.tsx
+++ src/components/markdown/editor/plugins.tsx
@@ -13,7 +13,7 @@ import {
     ListPlugin,
     NumberedListPlugin,
 } from '@udecode/plate-list/react';
-import { NodeIdPlugin } from '@udecode/plate-node-id';
+// import { NodeIdPlugin } from '@udecode/plate-node-id';
 import { ResetNodePlugin } from '@udecode/plate-reset-node/react';
 import { DeletePlugin } from '@udecode/plate-select';
 // import { TablePlugin } from '@udecode/plate-table/react';
@@ -120,7 +120,7 @@ export const articlePlugins = [
     }),
     VideoPlugin,
     DndPlugin,
-    NodeIdPlugin,
+    // NodeIdPlugin,
     /* TablePlugin.configure({
         options: {
             initialTableWidth: 600,
