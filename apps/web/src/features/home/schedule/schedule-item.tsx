255cb3a9c79f4eb42e4937ffc7a360e23795b765
olexh <typicaladmsky@gmail.com>
2025-06-05 03:26:42 +0300

    web/refactor: change schedule item style

--- apps/web/src/features/home/schedule/schedule-item.tsx
+++ apps/web/src/features/home/schedule/schedule-item.tsx
@@ -4,7 +4,14 @@ import { AnimeScheduleResponse } from '@hikka/client';
 import { FC, memo } from 'react';
 
 import Small from '@/components/typography/small';
-import HorizontalContentCard from '@/components/ui/horizontal-content-card';
+import Card from '@/components/ui/card';
+import {
+    HorizontalCard,
+    HorizontalCardContainer,
+    HorizontalCardDescription,
+    HorizontalCardImage,
+    HorizontalCardTitle,
+} from '@/components/ui/horizontal-card';
 
 import getScheduleDuration from '@/utils/get-schedule-duration';
 
@@ -14,17 +21,22 @@ interface Props {
 
 const ScheduleItem: FC<Props> = ({ item }) => {
     return (
-        <HorizontalContentCard
-            size="sm"
-            title={item.anime.title!}
-            description={getScheduleDuration(item.airing_at, item.time_left)}
-            href={`/anime/${item.anime.slug}`}
-            image={item.anime.image || undefined}
-        >
-            <Small className="text-muted-foreground opacity-60">
-                <span className="font-bold">{item.episode}</span> епізод
-            </Small>
-        </HorizontalContentCard>
+        <Card>
+            <HorizontalCard href={`/anime/${item.anime.slug}`}>
+                <HorizontalCardImage image={item.anime.image || undefined} />
+                <HorizontalCardContainer>
+                    <HorizontalCardTitle>
+                        {item.anime.title!}
+                    </HorizontalCardTitle>
+                    <HorizontalCardDescription>
+                        {getScheduleDuration(item.airing_at, item.time_left)}
+                    </HorizontalCardDescription>
+                    <Small className="text-muted-foreground opacity-60">
+                        <span className="font-bold">{item.episode}</span> епізод
+                    </Small>
+                </HorizontalCardContainer>
+            </HorizontalCard>
+        </Card>
     );
 };
 
