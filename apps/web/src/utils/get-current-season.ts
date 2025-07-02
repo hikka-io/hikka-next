cbbabc9763a596af617fd18564dc6ed0ec905045
olexh <typicaladmsky@gmail.com>
2025-07-02 19:13:19 +0300

    web/fix: shift current season month to 7 days back

--- apps/web/src/utils/get-current-season.ts
+++ apps/web/src/utils/get-current-season.ts
@@ -1,7 +1,9 @@
 import { SEASON } from './constants/common';
 
 export default function getCurrentSeason() {
-    const currentMonth = new Date().getMonth() + 1;
+    const adjustedDate = new Date();
+    adjustedDate.setDate(adjustedDate.getDate() - 7);
+    const currentMonth = adjustedDate.getMonth() + 1;
     return (Object.keys(SEASON) as Array<keyof typeof SEASON>).find((s) =>
         SEASON[s].params!.months.includes(currentMonth),
     );
