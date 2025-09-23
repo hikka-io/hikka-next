acdbfa597011dd08570ea0b3b8914de5f9b8704f
Lorg0n <lorgon.kv@gmail.com>
2025-09-23 14:57:54 +0300

    style(content): improve episode count display logic

--- apps/web/src/features/content/components/details/watch-details.tsx
+++ apps/web/src/features/content/components/details/watch-details.tsx
@@ -163,12 +163,12 @@ const WatchDetails = ({
                 icon={<Hash className="size-4" />}
                 title="Епізоди"
                 value={
-                    data.episodes_total && data.media_type !== 'movie'
+                    (data.episodes_total || data.episodes_released) && data.media_type !== 'movie'
                         ? formatEpisodeCount(
-                              data.status!,
-                              data.episodes_released,
-                              data.episodes_total,
-                          )
+                            data.status!,
+                            data.episodes_released,
+                            data.episodes_total,
+                        )
                         : undefined
                 }
             />
