c6fc24b1f158d6f8002c34cd4823580e3ada4476
olexh <typicaladmsky@gmail.com>
2025-10-23 00:37:47 +0300

    web/fix: movie banner timestamp

--- apps/web/src/utils/constants/banners.ts
+++ apps/web/src/utils/constants/banners.ts
@@ -11,6 +11,6 @@ export const MOVIE_BANNERS: MovieBanner[] = [
         slug: 'chainsaw-man-movie-reze-hen-c4febd',
         title: 'Вже ходили до кінотеатру?',
         description: 'Поділіться своїми враженнями!',
-        duration: [1761177600 /* 1760745600 */, 1762387200],
+        duration: [1761166800, 1762387200],
     },
 ];
