61a7b9359b51f1479eac4a4c35468ecd864b95a3
Lorg0n <lorgon.kv@gmail.com>
2025-11-08 16:26:46 +0300

    refactor(footer): refactor to use constants and dynamic links

--- apps/web/src/features/common/footer.tsx
+++ apps/web/src/features/common/footer.tsx
@@ -13,12 +13,10 @@ import { Label } from '@/components/ui/label';
 
 import { useModalContext } from '@/services/providers/modal-provider';
 
-import BxBxlGithub from '@/components/icons/bx/BxBxlGithub';
-import BxBxlTelegram from '@/components/icons/bx/BxBxlTelegram';
-import BxBxsDonateHeart from '@/components/icons/bx/BxBxsDonateHeart';
 import BxShareAlt from '@/components/icons/bx/BxShareAlt';
 import MaterialSymbolsFavoriteRounded from '@/components/icons/material-symbols/MaterialSymbolsFavoriteRounded';
-import PhTipJarFill from '@/components/icons/ph/PhTipJarFill';
+
+import { DONATION_LINKS, FOOTER_LINKS, SOCIAL_LINKS } from '@/utils/constants/footer';
 
 const Footer = () => {
     const { openModal } = useModalContext();
@@ -29,14 +27,14 @@ const Footer = () => {
                 <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
                     <div className="flex flex-wrap justify-center gap-4 lg:justify-start">
                         <Button variant="ghost" size="md" asChild>
-                            <Link href="/articles/pravyla-saytu-9bcf83">
-                                Правила
+                            <Link href={FOOTER_LINKS.rules.href}>
+                                {FOOTER_LINKS.rules.title}
                             </Link>
                         </Button>
 
                         <Button variant="ghost" size="md" asChild>
-                            <Link href="/articles/pravovlasnykam-a76512">
-                                Правовласникам
+                            <Link href={FOOTER_LINKS.owners.href}>
+                                {FOOTER_LINKS.owners.title}
                             </Link>
                         </Button>
 
@@ -48,27 +46,24 @@ const Footer = () => {
                                 </Button>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent>
-                                <DropdownMenuItem asChild>
-                                    <Link
-                                        href="https://donatello.to/hikka.io"
-                                        target="_blank"
-                                    >
-                                        <BxBxsDonateHeart className="mr-2 size-4" />
-                                        Donatello
-                                    </Link>
-                                </DropdownMenuItem>
-                                <DropdownMenuItem asChild>
-                                    <Link
-                                        href="https://send.monobank.ua/jar/UejmZHk4B"
-                                        target="_blank"
-                                        rel="noreferrer"
-                                    >
-                                        <PhTipJarFill className="mr-2 size-4" />
-                                        Монобанка
-                                    </Link>
-                                </DropdownMenuItem>
+                                {DONATION_LINKS.map((link) => {
+                                    const Icon = link.icon;
+                                    return (
+                                        <DropdownMenuItem key={link.href} asChild>
+                                            <Link
+                                                href={link.href}
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                <Icon className="mr-2 size-4" />
+                                                {link.title}
+                                            </Link>
+                                        </DropdownMenuItem>
+                                    );
+                                })}
                             </DropdownMenuContent>
                         </DropdownMenu>
+
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
                                 <Button variant="ghost" size="md">
@@ -77,24 +72,17 @@ const Footer = () => {
                                 </Button>
                             </DropdownMenuTrigger>
                             <DropdownMenuContent align="end">
-                                <DropdownMenuItem asChild>
-                                    <Link
-                                        href="https://t.me/hikka_io"
-                                        target="_blank"
-                                    >
-                                        <BxBxlTelegram className="mr-2 size-4" />
-                                        Telegram
-                                    </Link>
-                                </DropdownMenuItem>
-                                <DropdownMenuItem asChild>
-                                    <Link
-                                        href="https://github.com/hikka-io"
-                                        target="_blank"
-                                    >
-                                        <BxBxlGithub className="mr-2 size-4" />
-                                        GitHub
-                                    </Link>
-                                </DropdownMenuItem>
+                                {SOCIAL_LINKS.map((link) => {
+                                    const Icon = link.icon;
+                                    return (
+                                        <DropdownMenuItem key={link.href} asChild>
+                                            <Link href={link.href} target="_blank">
+                                                <Icon className="mr-2 size-4" />
+                                                {link.title}
+                                            </Link>
+                                        </DropdownMenuItem>
+                                    );
+                                })}
                             </DropdownMenuContent>
                         </DropdownMenu>
                     </div>
