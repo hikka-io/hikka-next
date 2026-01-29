a441e7b1fe5d4feafdfc2f62c3d9dfb63fb214a7
olexh <typicaladmsky@gmail.com>
2026-01-29 23:39:07 +0200

    web/feat: refactor edit card component to enhance layout and integrate Badge for status display

--- apps/web/src/features/modals/user-modals/components/edit-card.tsx
+++ apps/web/src/features/modals/user-modals/components/edit-card.tsx
@@ -2,14 +2,13 @@
 
 import { EditResponse, EditStatusEnum } from '@hikka/client';
 import { format } from 'date-fns';
-import Link from 'next/link';
 
 import MaterialSymbolsCheckRounded from '@/components/icons/material-symbols/MaterialSymbolsCheckRounded';
 import MaterialSymbolsCloseRounded from '@/components/icons/material-symbols/MaterialSymbolsCloseRounded';
 import MaterialSymbolsHourglassEmptyRounded from '@/components/icons/material-symbols/MaterialSymbolsHourglassEmptyRounded';
 import MaterialSymbolsVisibilityOutlineRounded from '@/components/icons/material-symbols/MaterialSymbolsVisibilityOutlineRounded';
 import Closed from '@/components/icons/watch-status/dropped';
-
+import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import {
     HorizontalCard,
@@ -19,6 +18,9 @@ import {
     HorizontalCardTitle,
 } from '@/components/ui/horizontal-card';
 
+import { cn } from '@/utils/cn';
+import { EDIT_PARAMS, EDIT_STATUS } from '@/utils/constants/edit';
+
 interface Props {
     edit: EditResponse;
     className?: string;
@@ -27,34 +29,33 @@ interface Props {
 
 const Component = ({ edit, href, className, ...props }: Props) => {
     return (
-        <HorizontalCard
-            href={`/u/${edit.author?.username}`}
-            className={className}
-        >
-            <HorizontalCardImage image={edit.author?.avatar} imageRatio={1} />
-            <HorizontalCardContainer>
-                <HorizontalCardTitle>
-                    {edit.author?.username}
-                </HorizontalCardTitle>
-                <HorizontalCardDescription>
-                    {format(edit.created * 1000, 'd MMM yyyy H:mm')}
-                </HorizontalCardDescription>
-            </HorizontalCardContainer>
-
-            <Button
-                asChild
-                size="icon-md"
-                variant={
-                    edit.status === EditStatusEnum.ACCEPTED
-                        ? 'success'
-                        : edit.status === EditStatusEnum.DENIED
-                            ? 'destructive'
-                            : edit.status === EditStatusEnum.CLOSED
+        <div className={cn('flex flex-col gap-4', className)}>
+            <HorizontalCard href={`/u/${edit.author?.username}`}>
+                <HorizontalCardImage
+                    className="w-10"
+                    image={edit.author?.avatar}
+                    imageRatio={1}
+                />
+                <HorizontalCardContainer className="shrink-0">
+                    <HorizontalCardTitle>
+                        {edit.author?.username}
+                    </HorizontalCardTitle>
+                    <HorizontalCardDescription>
+                        {format(edit.created * 1000, 'd MMM yyyy H:mm')}
+                    </HorizontalCardDescription>
+                </HorizontalCardContainer>
+                <Button
+                    size="md"
+                    variant={
+                        edit.status === EditStatusEnum.ACCEPTED
+                            ? 'success'
+                            : edit.status === EditStatusEnum.DENIED
+                              ? 'destructive'
+                              : edit.status === EditStatusEnum.CLOSED
                                 ? 'outline'
                                 : 'warning'
-                }
-            >
-                <Link href={href}>
+                    }
+                >
                     {edit.status === EditStatusEnum.ACCEPTED ? (
                         <MaterialSymbolsCheckRounded />
                     ) : edit.status === EditStatusEnum.DENIED ? (
@@ -66,9 +67,22 @@ const Component = ({ edit, href, className, ...props }: Props) => {
                     ) : (
                         <MaterialSymbolsVisibilityOutlineRounded />
                     )}
-                </Link>
-            </Button>
-        </HorizontalCard>
+                    <span className="hidden md:block">
+                        {EDIT_STATUS[edit.status].title_ua}
+                    </span>
+                </Button>
+            </HorizontalCard>
+            <div className="flex flex-wrap gap-2 border-l-2 pl-4">
+                {Object.keys(edit.after).map(
+                    (key) =>
+                        key !== 'title' && (
+                            <Badge variant="outline" key={key}>
+                                {EDIT_PARAMS[key as keyof typeof EDIT_PARAMS]}
+                            </Badge>
+                        ),
+                )}
+            </div>
+        </div>
     );
 };
 
