552fd8cc4b619c6a906f16d7f9c515f578b06031
olexh <typicaladmsky@gmail.com>
2026-02-19 19:19:20 +0200

    web/style: adjust padding in CollapsibleFilter component for improved layout consistency

--- apps/web/src/components/collapsible-filter.tsx
+++ apps/web/src/components/collapsible-filter.tsx
@@ -30,16 +30,15 @@ export const CollapsibleFilter: FC<CollapsibleFilterProps> = ({
         <Collapsible
             defaultOpen={active}
             className={cn(
-                'group border border-border bg-secondary/20 px-4 py-2 duration-200 data-[state=open]:mb-4 data-[state=open]:rounded-lg data-[state=open]:py-4',
+                'group border border-border bg-secondary/20 py-2 duration-200 data-[state=open]:mb-4 data-[state=open]:rounded-lg data-[state=open]:py-4',
                 '[&+div]:data-[state=open]:rounded-t-lg data-[state=open]:[&+div]:data-[state=closed]:rounded-b-lg',
                 'data-[state=closed]:border-b-0 data-[state=closed]:has-[+div[data-state=open]]:mb-4 data-[state=closed]:has-[+div[data-state=open]]:rounded-b-lg data-[state=closed]:has-[+div[data-state=open]]:border-b',
                 'first:rounded-t-lg last:rounded-b-lg last:!border-b',
-                className,
             )}
             {...props}
         >
             <CollapsibleTrigger asChild>
-                <div className="flex cursor-pointer items-center justify-between gap-2">
+                <div className="flex cursor-pointer items-center justify-between gap-2 px-4">
                     <div className="flex items-center gap-2">
                         {icon && icon}
                         <Label className="cursor-pointer select-none">
@@ -56,7 +55,12 @@ export const CollapsibleFilter: FC<CollapsibleFilterProps> = ({
                 </div>
             </CollapsibleTrigger>
 
-            <CollapsibleContent className="w-full overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down">
+            <CollapsibleContent
+                className={cn(
+                    'px-4 w-full overflow-hidden data-[state=closed]:animate-collapsible-up data-[state=open]:animate-collapsible-down',
+                    className,
+                )}
+            >
                 <div className="mt-4 space-y-4">{children}</div>
             </CollapsibleContent>
         </Collapsible>
