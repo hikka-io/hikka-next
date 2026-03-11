a0a92d13d82db23bc60ac0acdfbe529b8c391533
olexh <typicaladmsky@gmail.com>
2026-03-12 01:18:07 +0200

    web-start/feat: refactor alert dialog, dialog, and sheet components for improved state handling and styling

--- apps/web-start/src/components/ui/dialog.tsx
+++ apps/web-start/src/components/ui/dialog.tsx
@@ -21,7 +21,7 @@ const DialogOverlay = React.forwardRef<
     <DialogPrimitive.Overlay
         ref={ref}
         className={cn(
-            'fixed inset-0 z-50 grid max-h-screen place-items-center overflow-y-auto bg-black/30 backdrop-blur-xs data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+            'fixed inset-0 z-50 grid max-h-screen place-items-center overflow-y-auto bg-black/10 duration-100 supports-backdrop-filter:backdrop-blur-xs data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
             className,
         )}
         {...props}
@@ -61,7 +61,7 @@ const DialogContent = React.forwardRef<
                     <DialogPrimitive.Content
                         ref={ref}
                         className={cn(
-                            'relative z-50 grid w-full max-w-lg gap-4 border bg-background p-4 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:rounded-lg md:w-full',
+                            'relative z-50 grid w-full max-w-lg gap-4 ring-1 ring-foreground/10 bg-background p-4 duration-100 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 rounded-xl md:w-full',
                             className,
                         )}
                         {...props}
