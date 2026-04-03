3bacaaa30dddc096ad57c62c169de2d89b29c886
olexh <typicaladmsky@gmail.com>
2026-04-03 18:51:24 +0300

    client/feat: add feed filtering types and extended comment response
    Add new feed filter types for articles (content type, category) and
    sub-content-type filters for collections, comments, and articles.
    Introduce CommentResponseFeed with is_followed author field.
    Replace CommentResponse with CommentResponseFeed in FeedItemResponse.

--- packages/client/types/feed.ts
+++ packages/client/types/feed.ts
@@ -1,7 +1,12 @@
-import { ArticlePreviewResponse } from './articles';
-import { CollectionContent, CollectionResponse } from './collections';
-import { CommentResponse } from './comments';
+import { ArticleCategoryEnum, ArticlePreviewResponse } from './articles';
+import {
+    CollectionContent,
+    CollectionContentType,
+    CollectionResponse,
+} from './collections';
+import { CommentResponse, CommentsContentType } from './comments';
 import { ContentTypeEnum } from './common';
+import { UserResponse } from './user';
 
 /**
  * Feed content type filter
@@ -12,11 +17,43 @@ export type FeedContentType =
     | ContentTypeEnum.COMMENT;
 
 /**
+ * Article content type filter for feed (includes no_content)
+ */
+export type FeedArticleContentType =
+    | ContentTypeEnum.ANIME
+    | ContentTypeEnum.MANGA
+    | ContentTypeEnum.NOVEL
+    | 'no_content';
+
+/**
+ * Article category filter for feed
+ */
+export type FeedArticleCategory = Exclude<
+    ArticleCategoryEnum,
+    ArticleCategoryEnum.SYSTEM
+>;
+
+/**
  * Feed request arguments
  */
 export interface FeedArgs {
     before?: string | null;
     content_type?: FeedContentType | null;
+    collection_content_types?: CollectionContentType[] | null;
+    comment_content_types?: CommentsContentType[] | null;
+    article_content_types?: FeedArticleContentType[] | null;
+    article_categories?: FeedArticleCategory[] | null;
+    feed_content_types?: FeedContentType[] | null;
+    only_followed?: boolean;
+}
+
+/**
+ * Comment response specific to feed (author includes is_followed)
+ */
+export interface CommentResponseFeed
+    extends Omit<CommentResponse, 'author' | 'data_type'> {
+    data_type: ContentTypeEnum.COMMENT;
+    author: UserResponse & { is_followed: boolean };
 }
 
 /**
@@ -24,5 +61,5 @@ export interface FeedArgs {
  */
 export type FeedItemResponse =
     | ArticlePreviewResponse
-    | CommentResponse
+    | CommentResponseFeed
     | CollectionResponse<CollectionContent>;
