0f35997b9d4dd512b6bcbeabd0bd4280f33d5616
olexh <typicaladmsky@gmail.com>
2026-04-06 19:10:08 +0300

    react/fix: invalidate all notification queries on mark as seen

--- packages/react/client/hooks/api/notifications.ts
+++ packages/react/client/hooks/api/notifications.ts
@@ -66,5 +66,5 @@ export function useUnseenNotificationsCount({
 export const useUpdateNotificationSeen = createMutation({
     mutationFn: (client, reference: string) =>
         client.notifications.updateNotificationSeen(reference),
-    invalidateQueries: () => [queryKeys.notifications.unseenCount()],
+    invalidateQueries: () => [queryKeys.notifications.all],
 });
